7240e170098045f620a7e558e53f46b0e979567e
Guilherme Cantoni <104282001+gscantoni@users.noreply.github.com>
2023-09-10 15:08:56 -0300

    Update BibliotecaTest.java

--- Biblioteca/test/BibliotecaTest.java
+++ Biblioteca/test/BibliotecaTest.java
@@ -1,9 +1,54 @@
 package Biblioteca.test;
-import Biblioteca.src.Biblioteca;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.*;
+import Biblioteca.src.Biblioteca;
+import Biblioteca.src.Livro;
+import Biblioteca.src.Membro;
 
 public class BibliotecaTest {
-    
+    private Biblioteca biblioteca;
+    private Livro livro;
+    private Membro membro;
+
+    @Before
+    public void setUp() {
+        biblioteca = new Biblioteca();
+        livro = new Livro();
+        membro = new Membro(1, "João");
+    }
+
+    @Test
+    public void testRegistrarLivro() {
+        biblioteca.adicionarLivro(livro);
+        assertTrue(biblioteca.getLivros().contains(livro));
+    }
+
+    @Test
+    public void testEmprestarLivroParaMembro() {
+        biblioteca.adicionarLivro(livro);
+        assertTrue(livro.getEmprestado());
+
+        biblioteca.emprestarLivro(livro.getId(), membro.getId());
+        assertTrue(membro.listarLivrosEmprestados().contains(livro.getTitulo()));
+    }
+
+    @Test
+    public void testRetornarLivroDeMembro() {
+        biblioteca.adicionarLivro(livro);
+        biblioteca.emprestarLivro(livro.getId(), membro.getId());
+        assertTrue(membro.listarLivrosEmprestados().contains(livro.getTitulo()));
+
+        biblioteca.retornarLivro(livro.getId(), membro.getId());
+        assertFalse(membro.listarLivrosEmprestados().contains(livro.getTitulo()));
+    }
+
+    @Test
+    public void testEmprestarLivroNaoDisponivel() {
+        biblioteca.adicionarLivro(livro);
+        biblioteca.emprestarLivro(livro.getId(), membro.getId());
+
+        assertFalse(biblioteca.emprestarLivro(livro.getId(), membro.getId()));
+    }
 }
